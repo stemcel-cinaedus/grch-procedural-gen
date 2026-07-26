3b3444c810c3e2903e40623f408314ee458de303
Stone_Cold_Stemcel <stemboy@posteo.com>
2026-07-26 05:20:00 -0400

    Added construct_room function and builds a room in each leaf node tile

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,21 @@ struct BSPNode<T> {
     split_on_x: bool
 }
 
+
 impl BSPNode<Tile> {
+    fn random_split_helper_right(hw: Point2, tile_height: i64, tile_width: i64, s: bool) -> Point2 {
+        if (hw.1 as f64) / (hw.0 as f64) <= 0.2 {
+            return Point2((hw.0 - (tile_width as f64 * rand::random_range((2.0 / 5.0)..(3.0 / 5.0))) as i64), hw.1)
+        } else if (hw.1 as f64) / (hw.0 as f64) >= 5.0 {
+            return Point2(hw.0, (hw.1 - (tile_height as f64 * rand::random_range((2.0 / 5.0)..(3.0 / 5.0))) as i64))
+        } else if s == true {
+            return Point2((hw.0 - (tile_width as f64 * rand::random_range((2.0 / 5.0)..(3.0 / 5.0))) as i64), hw.1)
+        } else {
+            return Point2(hw.0, (hw.1 - (tile_height as f64 * rand::random_range((2.0 / 5.0)..(3.0 / 5.0))) as i64))
+        }
+    }
+
+
     fn split(&mut self) {
             match self.left  {
                 Some(_) => (),
@@ -23,11 +37,7 @@ impl BSPNode<Tile> {
                     self.left = Some(Box::from(BSPNode{
                         value: Tile { 
                             lc: self.value.lc,
-                            rc: if self.split_on_x == true {
-                                Point2((self.value.rc.0 as f64 * rand::random_range((1.0 / 5.0)..(4.0 / 5.0))) as i64, self.value.rc.1)
-                                } else {
-                                Point2(self.value.rc.0, (self.value.rc.1 as f64 * rand::random_range((1.0 / 5.0)..(4.0 / 5.0))) as i64)
-                                },
+                            rc: BSPNode::random_split_helper_right(self.value.rc, self.value.get_height(), self.value.get_width(), self.split_on_x ),
                             traversible: false,
                             split_count: (self.value.split_count + 1),
                             room: None
@@ -40,7 +50,13 @@ impl BSPNode<Tile> {
                 self.right = Some(Box::from(BSPNode{
                         value: Tile { 
                             //Add conditional to make the tiles squares instead of line segments
-                            lc: self.left.as_ref().unwrap().value.rc,
+                            
+                            lc: if self.split_on_x == true {
+                                Point2(self.left.as_ref().unwrap().value.rc.0, self.left.as_ref().unwrap().value.lc.1) 
+                            } else {
+                                Point2(self.left.as_ref().unwrap().value.lc.0, self.left.as_ref().unwrap().value.rc.1) 
+                            },
+                                
                             rc: self.value.rc,
                             traversible: false,
                             split_count: (self.value.split_count + 1),
@@ -131,18 +147,45 @@ fn split_dfs(root: &mut BSPNode<Tile>, depth: i64) {
     }
 }
 
+fn construct_room(tile: Tile) -> Room {
+    let dist_from_lc: i64 = rand::random_range(1..3);
+    let dist_from_rc: i64 = rand::random_range(1..3);
+    return Room(
+        Point2(tile.rc.0 - dist_from_lc, tile.rc.1 - dist_from_lc),
+        Point2(tile.rc.0 - dist_from_rc, tile.rc.1 - dist_from_rc),
+        true )
+}
+
 fn build_dfs(root: BSPNode<Tile>, map: &mut Map) -> () {
     if root.right != None {
         build_dfs(*root.right.unwrap(), map);
         build_dfs(*root.left.unwrap(), map);
         } else {
-        map.tiles.push(Tile{lc: root.value.lc, rc: root.value.rc, traversible: true, split_count: root.value.split_count, room: Some(Room(Point2(0, 0), Point2(0, 0), false))})
+        map.tiles.push(Tile{
+            lc: root.value.lc,
+            rc: root.value.rc,
+            traversible: true,
+            split_count: root.value.split_count,
+            room: Some(construct_room(Tile {
+                lc: (root.value.lc),
+                rc: (root.value.rc),
+                traversible: true,
+                split_count: root.value.split_count,
+                room: None
+            }))
+        })
     }
 }
 
 
 pub fn initbt(size: Point2, divisions: i64) -> () {
-    let mut root = BSPNode{ value: Tile{lc: Point2(0, 0), rc: size, traversible: false, split_count: 0, room: None}, right: None, left: None, room: None, split_on_x: rand::random_bool(1.0/2.0)};
+    let mut root = BSPNode{
+        value: Tile{lc: Point2(0, 0), rc: size, traversible: false, split_count: 0, room: None},
+        right: None,
+        left: None,
+        room: None,
+        split_on_x: rand::random_bool(1.0/2.0)
+    };
     split_dfs(&mut root, divisions);
     let mut map = Map{max_height: 512, max_width: 512, tiles: Vec::<Tile>::new()};
     build_dfs(root, &mut map);
@@ -154,7 +197,7 @@ pub fn initbt(size: Point2, divisions: i64) -> () {
 
 fn main() {
     let divisions: i64 = 4;
-    let mut root = BSPNode{ value: Tile{lc: Point2(0,0), rc: Point2(-512, -512), traversible: false, split_count: 0, room: None}, right: None, left: None, room: None, split_on_x: rand::random_bool(1.0/2.0)};
+    let mut root = BSPNode{ value: Tile{lc: Point2(0,0), rc: Point2(512, 512), traversible: false, split_count: 0, room: None}, right: None, left: None, room: None, split_on_x: rand::random_bool(1.0/2.0)};
     split_dfs(&mut root, divisions);
     let mut map = Map{max_height: 512, max_width: 512, tiles: Vec::<Tile>::new()};
     build_dfs(root, &mut map);
