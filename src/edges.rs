987e50d923e1fa5896220c1b633933227b488fe9
stemcel-cinaedus <187351941+stemcel-cinaedus@users.noreply.github.com>
2026-07-29 23:02:09 -0400

    Add files via upload
    Big refactor to reduce total LoC, changed functions to allow rooms to have indices

--- src/edges.rs
+++ src/edges.rs
@@ -1,10 +1,5 @@
-use std::cell::RefCell;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize};
 use std::sync::{Arc, RwLock};
-use std::thread;
-
-
 
 use crate::PLACE_DIST;
 use crate::CORRIDOR_OFFSET;
@@ -29,7 +24,7 @@ fn generate_candidates(corner: Point3, bounding_corner: Point3, axis: Axis) -> V
 
             while y.abs() < (y.abs() + y_range.abs()) && z.abs() < (z.abs() + z_range.abs()) {
                 //TODO: I want to change this to check if it's within bounds first
-                candidates.push(Point3(corner.0, (y + CORRIDOR_OFFSET.1), (z + CORRIDOR_OFFSET.2)));
+                candidates.push(Point3(corner.0, y + CORRIDOR_OFFSET.1 , z + CORRIDOR_OFFSET.2 ));
                 //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
                 z += PLACE_DIST;
             }
@@ -48,7 +43,7 @@ fn generate_candidates(corner: Point3, bounding_corner: Point3, axis: Axis) -> V
 
             while y.abs() < (y.abs() + y_range.abs()) && x.abs() < (x.abs() + x_range.abs()) {
                 //TODO: I want to change this to check if it's within bounds first
-                candidates.push(Point3((x + CORRIDOR_OFFSET.0), (y + CORRIDOR_OFFSET.1), corner.2));
+                candidates.push(Point3(x + CORRIDOR_OFFSET.0, y + CORRIDOR_OFFSET.1, corner.2));
                 //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
                 x += PLACE_DIST;
             }
@@ -57,13 +52,10 @@ fn generate_candidates(corner: Point3, bounding_corner: Point3, axis: Axis) -> V
     }
 }
 
-fn generate_edges(rooms: (usize, Room, Room)) -> (usize, Point3, Point3) {
-    let (lc1, rc1) = (rooms.1.0, rooms.1.1);
-    let (lc2, rc2) = (rooms.2.0, rooms.2.1);
-    //Point P = x_0, y_0, z_0; Normal vector N = <a,b,c>, gen eqn: a(x - x_0) + b(y - y_0) + c(z - z_0) = 0
-    //left_face_plane: -1(x - lc.0) + 0(y - y0) + 0(z - z0) = 0
-    //left_face_plane: -1(x - lc.0) = 0
-    //left_face_plane: -x + lc.0 = 0
+fn generate_edges(rooms: (Room, Room)) -> ((usize, Point3, Point3), (usize, Point3, Point3)) {
+    let (lc1, rc1) = (rooms.0.1, rooms.0.2);
+    let (lc2, rc2) = (rooms.1.1, rooms.1.2);
+    let indices = (rooms.0.0, rooms.1.0);
 
     let mut candidates1  = Vec::<_>::new();
     let mut candidates2  = Vec::<_>::new();
@@ -97,7 +89,7 @@ fn generate_edges(rooms: (usize, Room, Room)) -> (usize, Point3, Point3) {
     //TODO: Implement distance algorithm for each candidate array, ideally in O(k log_k), somehow a hard task
 
     let mut shortest =  f64::INFINITY;
-    let mut shortest_points = (INDEX.fetch_add(1, Ordering::Relaxed), Point3(0,0,0), Point3(0,0,0));
+    let mut shortest_points = (Point3(0,0,0), Point3(0,0,0));
 
     let mut c1 = Vec::<Point3>::new();
     let mut c2 = Vec::<Point3>::new();
@@ -109,14 +101,17 @@ fn generate_edges(rooms: (usize, Room, Room)) -> (usize, Point3, Point3) {
             let dist = ((e1.0 as f64 + e2.0 as f64).powf(2.0) + (e1.1 as f64 + e2.1 as f64).powf(2.0) + (e1.2 as f64 + e2.2 as f64).powf(2.0)).sqrt();
             if dist < shortest {
                 shortest = dist;
-                shortest_points = (shortest_points.0, *e1, *e2); 
+                shortest_points = (*e1, *e2); 
             }
         }
     }
+    let shortest_points = ((indices.0, shortest_points.0, shortest_points.1), (indices.1, shortest_points.0, shortest_points.1));
     return shortest_points
 }
 
 fn get_groups(root: &BSPNode<Tile>, divisions: i64) {
+    let mut rooms_in_grandparent = Arc::new(RwLock::new(Vec::<Room>::new()));
+
     while root.value.split_count > 2 {
         get_groups(&(root.right.as_deref().unwrap()), divisions);
         get_groups(&(root.left.as_deref().unwrap()), divisions);
@@ -131,48 +126,40 @@ fn get_groups(root: &BSPNode<Tile>, divisions: i64) {
         }
     }
 
-    let mut rooms_in_grandparent = Vec::<Room>::new();
-
     //TODO: Use better connection method. Using the first Room found in the grandparent is retarded.
+    fn match_get_leaf(root: &BSPNode<Tile>, rooms_in_grandparent: &mut Arc<RwLock<Vec<Room>>>) {
+            match get_leaf_rooms(root) {
+            (Some(r1), Some(r2)) => {
+                let e = generate_edges((r1, r2));
+                EDGES.write().unwrap().push(e.0);
+                EDGES.write().unwrap().push(e.1);
+                rooms_in_grandparent.write().unwrap().push(r1);
+                rooms_in_grandparent.write().unwrap().push(r2);},
+            (Some(r1), None) => {
+                if !rooms_in_grandparent.read().unwrap().is_empty() {
+                    let e = generate_edges((r1, rooms_in_grandparent.read().unwrap()[0]));
+                    EDGES.write().unwrap().push(e.0);
+                    EDGES.write().unwrap().push(e.1);
+                }
+                rooms_in_grandparent.write().unwrap().push(r1);
+            },
+            (None, Some(r2)) => {
+                if !rooms_in_grandparent.read().unwrap().is_empty() {
+                    let e = generate_edges((r2, rooms_in_grandparent.read().unwrap()[0]));
+                    EDGES.write().unwrap().push(e.0);
+                    EDGES.write().unwrap().push(e.1);
+                }
+                rooms_in_grandparent.write().unwrap().push(r2);
+            },
+            (None, None) => () 
+        }
+    }
 
-    match get_leaf_rooms(&(root.left.as_deref().unwrap())) {
-        (Some(r1), Some(r2)) => {
-            EDGES.write().unwrap().push(generate_edges((INDEX.fetch_add(1, Ordering::Relaxed), r1, r2)));
-            rooms_in_grandparent.push(r1);
-            rooms_in_grandparent.push(r2);},
-        (Some(r1), None) => {
-            if !rooms_in_grandparent.is_empty() {
-                EDGES.write().unwrap().push(generate_edges((INDEX.fetch_add(1, Ordering::Relaxed), r1, rooms_in_grandparent[0])))
-            }
-            rooms_in_grandparent.push(r1);
-        },
-        (None, Some(r2)) => {
-            if !rooms_in_grandparent.is_empty() {
-                EDGES.write().unwrap().push(generate_edges((INDEX.fetch_add(1, Ordering::Relaxed), r2, rooms_in_grandparent[0])))
-            }
-            rooms_in_grandparent.push(r2);
-        },
-        (None, None) => () 
-    }
-    match get_leaf_rooms(&(root.right.as_deref().unwrap())) {
-        (Some(r1), Some(r2)) => {
-            EDGES.write().unwrap().push(generate_edges((INDEX.fetch_add(1, Ordering::Relaxed), r1, r2)));
-            rooms_in_grandparent.push(r1);
-            rooms_in_grandparent.push(r2);},
-        (Some(r1), None) => {
-            if !rooms_in_grandparent.is_empty() {
-                EDGES.write().unwrap().push(generate_edges((INDEX.fetch_add(1, Ordering::Relaxed), r1, rooms_in_grandparent[0])))
-            }
-            rooms_in_grandparent.push(r1);
-        },
-        (None, Some(r2)) => {
-            if !rooms_in_grandparent.is_empty() {
-                EDGES.write().unwrap().push(generate_edges((INDEX.fetch_add(1, Ordering::Relaxed), r2, rooms_in_grandparent[0])))
-            }
-            rooms_in_grandparent.push(r2);
-        },
-        (None, None) => () 
-    }
+    
+
+    match_get_leaf(&(root.left.as_deref().unwrap()), &mut rooms_in_grandparent);
+    match_get_leaf(&(root.right.as_deref().unwrap()), &mut rooms_in_grandparent);
+
 }
 
 
@@ -187,13 +174,13 @@ fn union_find(rooms: Vec::<Room>) -> i32 {
          vert_map.insert(a, b);
         });
 
-    let mut parents: Vec<i32> = vec![0; vert_map.len()];
+    let mut parents: Vec<usize> = vec![0; vert_map.len()];
     let mut rank: Vec<i32> = vec![0; vert_map.len()];
 
-    fn find(node1: (i32, Room), parents: &mut Vec<i32>) -> i32 {
+    fn find(node1: (usize, Room), parents: &mut Vec<usize>) -> usize {
         let mut res = node1.0;
         
-        while res != parents[res as usize] as i32 {
+        while res != parents[res as usize] {
             parents[res as usize] = parents[parents[res as usize] as usize];
             res = parents[res as usize];
            // vert_map[res.0] = vert_map[vert_map.contains_key(res.0)]
@@ -201,7 +188,7 @@ fn union_find(rooms: Vec::<Room>) -> i32 {
         return res
     }
 
-    fn union(c1: (i32, Room), c2: (i32, Room), mut parents: Vec<i32>, mut rank: Vec<i32>) -> i32{
+    fn union(c1: (usize, Room), c2: (usize, Room), mut parents: Vec<usize>, mut rank: Vec<i32>) -> usize{
         let p1 = find(c1, &mut parents);
         let p2 = find(c2, &mut parents);
 
@@ -219,8 +206,8 @@ fn union_find(rooms: Vec::<Room>) -> i32 {
     }
 
     /*
-    let mut result: i32 = vert_map.len() as i32;
-    for (n1, n2) in edges {
+    let mut result: usize = vert_map.len();
+    for (i, n1, n2) in EDGES.read().unwrap().iter() {
         result -= union(n1, n2, parents, rank);
     } 
     return result
