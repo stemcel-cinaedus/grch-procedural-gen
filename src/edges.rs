44b21599515d3eca2c1ad689e0217f9b0278306e
stemcel-cinaedus <187351941+stemcel-cinaedus@users.noreply.github.com>
2026-08-02 23:47:32 -0400

    Add files via upload

--- src/edges.rs
+++ src/edges.rs
@@ -10,78 +10,139 @@ pub static EDGES: RwLock<Vec<(Point3, Point3)>> = RwLock::new(Vec::<(Point3, Poi
 pub static INDEX: AtomicUsize = AtomicUsize::new(0);
 
 //Make Megumi holes
-fn generate_candidates(corner: Point3, bounding_corner: Point3, axis: Axis) -> Vec<Point3> {
+fn generate_candidates(lc: Point3, rc: Point3, axis: Axis, split_pos: Point3) -> Vec<Point3> {
+
     match axis {
         Axis::X => {
             //Start at lowest Z & lowest Y value, move along the face of the shape to create more every d distance
-            let y_range = bounding_corner.1;
-            let z_range = bounding_corner.2;
 
-            let y = corner.1;
-            let mut z = corner.2;
+            //ATTENTION! This causes intersections through boxes because the logic is bad. Add a conditional to fix it.
+            if lc.0 > split_pos.0 || lc.1 > split_pos.1 || lc.2 > split_pos.2 {
+                let y_range = rc.1;
+                let z_range = rc.2;
+                let y = lc.1;
+                let mut z = lc.2;
+                let mut candidates = Vec::<Point3>::new();
 
-            let mut candidates = Vec::<Point3>::new();
+                while y + CORRIDOR_OFFSET.1 < y_range && z + CORRIDOR_OFFSET.2 < z_range {
+                    //TODO: I want to change this to check if it's within bounds first
+                    candidates.push(Point3(lc.0, y + CORRIDOR_OFFSET.1 , z + CORRIDOR_OFFSET.2 ));
+                    //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
+                    z += PLACE_DIST
+                }
+                return candidates;
+            } else {
+                let y_range = lc.1;
+                let z_range = lc.2;
+                let y = rc.1;
+                let mut z = rc.2;
+                let mut candidates = Vec::<Point3>::new();
 
-            while y.abs() < (y_range.abs()) && z.abs() < (z_range.abs()) {
-                //TODO: I want to change this to check if it's within bounds first
-                candidates.push(Point3(corner.0, y + CORRIDOR_OFFSET.1 , z + CORRIDOR_OFFSET.2 ));
-                //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
-                z += PLACE_DIST;
+                while y + CORRIDOR_OFFSET.1 > y_range && z + CORRIDOR_OFFSET.2 > z_range {
+                    //TODO: I want to change this to check if it's within bounds first
+                    candidates.push(Point3(lc.0, y + CORRIDOR_OFFSET.1 , z + CORRIDOR_OFFSET.2 ));
+                    //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
+                    z -= PLACE_DIST
+                }
+                return candidates;
             }
-            return candidates;
-
         }, //Add steepness check later
         Axis::Y => {return Vec::<Point3>::new()}, //Add vertical corridors later
         Axis::Z => {
-            let x_range = bounding_corner.0;
-            let y_range = bounding_corner.1;
-            
-            let mut x = corner.0;
-            let y = corner.1;
-            
-            let mut candidates = Vec::<Point3>::new();
+            if lc.0 > split_pos.0 || lc.1 > split_pos.1 || lc.2 > split_pos.2 {
+                let y_range = rc.1;
+                let x_range = rc.0;
+                let y = lc.1;
+                let mut x = lc.0;
+                let mut candidates = Vec::<Point3>::new();
 
-            while y.abs() < y_range.abs() && x.abs() < x_range.abs() {
-                //TODO: I want to change this to check if it's within bounds first
-                candidates.push(Point3(x + CORRIDOR_OFFSET.0, y + CORRIDOR_OFFSET.1, corner.2));
-                //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
-                x += PLACE_DIST;
+                while y + CORRIDOR_OFFSET.1 < y_range && x + CORRIDOR_OFFSET.0 < x_range {
+                    //TODO: I want to change this to check if it's within bounds first
+                    candidates.push(Point3(lc.0, y + CORRIDOR_OFFSET.1 , x + CORRIDOR_OFFSET.0 ));
+                    //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
+                    x += PLACE_DIST
+                }
+                return candidates;
+            } else {
+                let y_range = lc.1;
+                let x_range = lc.0;
+                let y = rc.1;
+                let mut x = rc.0;
+                let mut candidates = Vec::<Point3>::new();
+
+                while y + CORRIDOR_OFFSET.1 > y_range && x + CORRIDOR_OFFSET.0 > x_range {
+                    //TODO: I want to change this to check if it's within bounds first
+                    candidates.push(Point3(lc.0, y + CORRIDOR_OFFSET.1 , x + CORRIDOR_OFFSET.0 ));
+                    //Currently, I am only placing candidates at the lowest Y level to get a working version first. Later, I will add variable Y as well.
+                    x -= PLACE_DIST
+                }
+                return candidates;
             }
-            return candidates;
         }
     }
 }
 
-fn generate_edges(rooms: (&[Room], &[Room]), axis: Axis, split_pos: Point3) -> () {
+fn generate_edges(rooms: (&[&Room], &[&Room]), axis: Axis, split_pos: Point3) -> () {
     //Refactor to make it find the rooms with the room positions closest to the split , 
     //Lazy to implement rn, but basically it would be like match the plane & split point, and then grab the top n rooms on the left that are closest to the split,
     //grab the top n rooms on the right that are closest to the split, and run generate candidates on them for a total of n*n calculations, saving a lot more resources than just 
     //checking every possible vertice.
 
-    fn get_closest(rooms: &[Room], split_pos: Point3, axis: Axis, n: usize) -> impl Iterator<Item = &Room> {
+    fn get_closest<'a>(rooms: &'a [&Room], split_pos: Point3, split_d: bool, axis: Axis, n: usize) -> impl Iterator<Item = &'a&'a Room> {
         
         //Probably will change "closest_room" to be a tuple or maybe a Vec of length n so that I can grab the 1st, 2nd, etc closest
+        //Needs to be fixed to ensure rooms aren't across the map
         match axis {
             Axis::X => {
-                let mut closest_rooms: Vec<&Room> = rooms.iter().collect();
-                closest_rooms.sort_by_key(|room| {
-                    return (split_pos.0 - room.1.0).abs().min((split_pos.0 - room.2.0).abs());
-                });
-                return closest_rooms.into_iter().take(n);                
+                //Using .abs() here will return anything on either side of the split point, which will lead to clipping through rooms and candidates being placed on the wrong face
+                //A conditional must be added, I will also most likely add another parameter to the function that the conditional matches against. This still wouldn't necessarily fix edges
+                //clipping through rooms, eg. if the two closest rooms were along the same axis to the split. There is no easy modification I can think of to solve this, other than maybe adding
+                //some kind of check that says room n+1 must be offset by multiple dimensions, but this still isn't perfect.
+
+                //Added conditional, will need a better solution later.
+                if split_d == true {
+                    let mut closest_rooms: Vec<_> = rooms.iter().collect();
+                    closest_rooms.sort_by_key(|room| {
+                        return (room.1.0 - split_pos.0).min(room.2.0 - split_pos.0);
+                    });
+                    return closest_rooms.into_iter().take(n);
+                }   else {
+                    let mut closest_rooms: Vec<_> = rooms.iter().collect();
+                    closest_rooms.sort_by_key(|room| {
+                        return (split_pos.0 - room.1.0).min(split_pos.0 - room.2.0);
+                    });
+                    return closest_rooms.into_iter().take(n);
+                }
             }
             Axis::Y => {
-                let mut closest_rooms: Vec<&Room> = rooms.iter().collect();
-                closest_rooms.sort_by_key(|room| {
-                    return (split_pos.1 - room.1.1).abs().min((split_pos.1 - room.2.1).abs());
-                });
-                return closest_rooms.into_iter().take(n);                
+                if split_d == true {
+                    let mut closest_rooms: Vec<_> = rooms.iter().collect();
+                    closest_rooms.sort_by_key(|room| {
+                        return (room.1.1 - split_pos.1).min(room.2.1 - split_pos.1);
+                    });
+                    return closest_rooms.into_iter().take(n);
+                }   else {
+                    let mut closest_rooms: Vec<_> = rooms.iter().collect();
+                    closest_rooms.sort_by_key(|room| {
+                        return (split_pos.1 - room.1.1).min(split_pos.1 - room.2.1);
+                    });
+                    return closest_rooms.into_iter().take(n);
+                }               
             }
             Axis::Z => {
-                let mut closest_rooms: Vec<&Room> = rooms.iter().collect();
-                closest_rooms.sort_by_key(|room| {
-                    return (split_pos.2 - room.1.2).abs().min((split_pos.2 - room.2.2).abs());
-                });
-                return closest_rooms.into_iter().take(n);                
+               if split_d == true {
+                    let mut closest_rooms: Vec<_> = rooms.iter().collect();
+                    closest_rooms.sort_by_key(|room| {
+                        return (room.1.2 - split_pos.2).min(room.2.2 - split_pos.2);
+                    });
+                    return closest_rooms.into_iter().take(n);
+                }   else {
+                    let mut closest_rooms: Vec<_> = rooms.iter().collect();
+                    closest_rooms.sort_by_key(|room| {
+                        return (split_pos.2 - room.1.2).min(split_pos.2 - room.2.2);
+                    });
+                    return closest_rooms.into_iter().take(n);
+                }                
             }
         }
     }
@@ -90,12 +151,18 @@ fn generate_edges(rooms: (&[Room], &[Room]), axis: Axis, split_pos: Point3) -> (
 
     //Now the idea is to take the n closest and draw corridors between them
     //1 for testing currently
-    let left_candidates = get_closest(rooms.0, split_pos, axis, 1).map(|room| generate_candidates(room.1, room.2, axis));
-    let right_candidates = get_closest(rooms.1, split_pos, axis, 1).map(|room| generate_candidates(room.2, room.1, axis));
+    let left_candidates = get_closest(rooms.0, split_pos, false, axis, 1).map(|room| generate_candidates(room.1, room.2, axis, split_pos));
+    let right_candidates = get_closest(rooms.1, split_pos, true, axis, 1).map(|room| generate_candidates(room.1, room.2, axis, split_pos));
    
+    //This is returning the maximum sometimes, maybe it is receiving empty vecs? I will either add a check or look
+
     for (c1, c2) in left_candidates.zip(right_candidates) {
         let mut shortest =  i64::MAX;
         let mut shortest_points = (Point3(i64::MAX,i64::MAX,i64::MAX), Point3(i64::MAX,i64::MAX,i64::MAX));
+
+        if (c1.is_empty() || c2.is_empty()) {
+            return;
+        }
 
         for e1 in &c1 {
             for e2 in &c2 {
@@ -116,22 +183,22 @@ fn generate_edges(rooms: (&[Room], &[Room]), axis: Axis, split_pos: Point3) -> (
 
 */
 
-pub fn edge_dfs(root: &BSPNode<Tile>, divisions: i64) -> Vec<Room> {
-
-    //TRASH DOESNT WORK, NEED TO CHANGE GENERATE EDGES TO TAKE VARIADIC INPUT WITH VECS 
-    let mut left_rooms = Vec::new();
-    let mut right_rooms = Vec::new();
-
+pub fn edge_dfs(root: &BSPNode<Tile>, divisions: i64) -> Vec<&Room> {
     if divisions - root.value.split_count >= 1 {
-        left_rooms = edge_dfs(&(root.left.as_deref().unwrap()), divisions);
-        right_rooms = edge_dfs(&(root.right.as_deref().unwrap()), divisions);
-        generate_edges((&left_rooms, &right_rooms), root.split_d, (root.left.as_deref().unwrap()).value.rc);  
+        let mut left_rooms:Vec<&Room> = edge_dfs(&(root.left.as_deref().unwrap()), divisions);
+        let right_rooms:Vec<&Room>  = edge_dfs(&(root.right.as_deref().unwrap()), divisions);
+        generate_edges((&left_rooms, &right_rooms), root.split_d, (root.left.as_deref().unwrap()).value.rc);
+        left_rooms.extend(right_rooms);
+        return left_rooms
     }  else {
+        let mut rooms = Vec::<&Room>::new();
         if root.value.room.is_some() {
-            left_rooms.push(root.value.room.unwrap());
+            rooms.push(&(root.value.room.as_ref().unwrap()));
+            return rooms
+        } else {
+            return rooms
         }
     }
-    return left_rooms.into_iter().chain(right_rooms).collect();
 }
 
 
